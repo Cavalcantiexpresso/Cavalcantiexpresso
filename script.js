08e8900736561bf3a79495659da4137ce42d912f
Luciano Paiva Cavalcanti <98571732+Cavalcantiexpresso@users.noreply.github.com>
2022-10-05 02:53:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+
+const jump = () => {
+  mario.classList.add("jump");
+
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+};
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetleft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ' ')
+
+  console.log(marioPosition);
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    
+    pipe.style.animation = 'nome';
+    pipe.style.left = `${pipePosition}px`;
+  }
+
+}, 10);
+
+document.addEventListener("keydown", jump);
